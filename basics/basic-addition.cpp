d2724795fbc2e9c0a089711f096274cb50b69525
Amiel Josh Basug <amieljosh05@gmail.com>
2025-12-19 00:35:24 +0800

    basics

---
+++ basics/basic-addition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+
+    cout << "Enter 1st number: ";
+    cin  >> a;
+
+    cout << "Enter 2nd number: ";
+    cin >> b;
+
+    cout << "Sum = " << (a + b) << endl;
+    return 0;
+}
